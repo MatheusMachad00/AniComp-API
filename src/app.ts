0fbb5d67cd12f7b76c616b816ceb42fd5eb70d96
MatheusMachad00 <matheus_m_machado@hotmail.com>
2024-05-08 14:23:37 -0300

    Feat: signup and login on air

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import 'express-async-errors';
+import "express-async-errors";
+import router from "./router/index";
+import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware";
 
 dotenv.config();
 const app = express();
 app.use(cors(), express.json());
+app.use(router);
+app.use(errorHandlerMiddleware);
 
 export default app;
---
+++ src/service/authService.ts
@@ -0,0 +1,58 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { TypeNewUserData, TypeNewLogin } from "../type/userType";
+import { authRepository } from "../repository/authRepository";
+
+async function signup(signupData: TypeNewUserData) {
+  const { username, email, password } = signupData;
+  const SALT = 10;
+  const encryptedPassword = bcrypt.hashSync(password, SALT);
+
+  const emailExists = await checkEmail(email);
+  if (emailExists)
+    throw { type: "conflict", message: "Email already registered." };
+
+  const usernameExists = await authRepository.checkUsername(username);
+  if (usernameExists)
+    throw { type: "conflict", message: "Username already registered." };
+
+  await authRepository.createUser({
+    ...signupData,
+    password: encryptedPassword,
+  });
+}
+
+async function login(login: TypeNewLogin) {
+  const KEY_JWT = process.env.JWT_SECRET;
+  const data = login;
+
+  const isEmailValid = checkEmail(data.email);
+  if (!isEmailValid)
+    throw { type: "conflict", message: "Email already registered." };
+
+  const { id, username, email, avatar } = await getUserOrFail(data);
+  const token = jwt.sign({ id, username, email, avatar }, String(KEY_JWT));
+
+  return { token, id, username, avatar };
+}
+
+async function checkEmail(email: string) {
+  const emailExists = await authRepository.checkEmail(email);
+  return emailExists;
+}
+
+async function getUserOrFail(login: TypeNewLogin) {
+  const user = await authRepository.checkEmail(login.email);
+  if (!user) throw { type: "unauthorized", message: "Wrong email or password" };
+
+  const isPasswordValid = bcrypt.compareSync(login.password, user.password);
+  if (!isPasswordValid)
+    throw { type: "unauthorized", message: "Wrong email or password" };
+
+  return user;
+}
+
+export const authService = {
+  signup,
+  login,
+};
